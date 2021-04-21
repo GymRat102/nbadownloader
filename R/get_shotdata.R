b167bf765440a50cc06372444dc4b6992e2b6f9d
Weijian_Duan <weijianduan0302@gmail.com>
2021-04-21 16:56:37 +0800

    fix the example to right format

--- R/get_shotdata.R
+++ R/get_shotdata.R
@@ -6,7 +6,7 @@
 #' @export
 #'
 #' @example
-#' get_shotdata("2020-21", "201939") --- 201939 is Stephen Curry's playerId on nba.com
+#' get_shotdata("2020-21", "201939")
 get_shotdata <- function(season, playerId){
   headers = c(
     `Connection` = 'keep-alive',
